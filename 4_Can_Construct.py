7e338b685f57afa02641faf5808c5ca6ee0bd7fc
Yujia Cheng <sapphirechengyj@gmail.com>
2022-03-10 20:41:06 -0800

    finishes implementation of canConstruct memoization and improves string editing part of the code

--- 4_Can_Construct.py
+++ 4_Can_Construct.py
@@ -1,22 +1,56 @@
+from operator import truediv
+
+
 def canConstruct(targetWord, wordbank):
+    """
+    targetWord length = m
+    wordbank length = n
+    time complexity = O(n^m*m) multiplying m due to spliting operation
+    space complexity O(m^2)
+    """
     if targetWord == '':
         return True
     
     else:
         for word in wordbank: 
-            if word in targetWord:
-                if targetWord.index(word) == 0:
-                    suffix = targetWord.strip(word)
-                    result = canConstruct(suffix, wordbank)
-                    if result:
-                        return True
+            if word in targetWord and targetWord.index(word) == 0: 
+                suffix = targetWord.replace(word, "", 1)  # only let the replacment happen once
+                result = canConstruct(suffix, wordbank)
+                if result:
+                    return True
         
+        return False
+
+def canConstruct_memoized(targetWord, wordbank, memo = {}):
+    """
+    targetWord length = m
+    wordbank length = n
+    time complexity = O(n*m^2) multiplying m due to spliting operation
+    space complexity O(m^2)
+    """
+    if targetWord in memo:
+        return memo[targetWord]
+
+    elif targetWord == '':
+        return True
+    
+    else:
+        for word in wordbank: 
+            if word in targetWord and targetWord.index(word) == 0:
+                suffix = targetWord.replace(word, "", 1)  # only let the replacment happen once
+                result = canConstruct_memoized(suffix, wordbank, memo)  
+                if result:
+                    memo[targetWord] = True
+                    return True
+        
+        memo[targetWord] = False
         return False
     
 def main():
-    targetWord = 'albado'
-    wordbank = ['ab', 'abc', 'cd', 'def', 'abcd']
-    print(canConstruct(targetWord, wordbank))
+    targetWord = 'eeeeeeeeeeeeeeef'
+    wordbank = ['e', 'ee', 'eee', 'eeee', 'eeeeef']
+    print(canConstruct_memoized(targetWord, wordbank))
+
 
 if __name__ == '__main__':
     main()
